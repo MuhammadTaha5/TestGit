82cc798b6f675dbe2550ccd36176b9af9bab77b0
MuhammadTaha5 <taha.saeed339@gmail.com>
2026-03-27 16:51:29 +0500

    Add function in code

--- code.py
+++ code.py
@@ -1,5 +1,5 @@
 # -----------------------Task 1--------------------------
-
+# Logs data from the dataset
     
 login_logs = [
 ("ali","192.168.1.2","failed"),
@@ -19,27 +19,55 @@ login_logs = [
 
 print()
 print("---------------------------Task1----------------------------")
-# Dictionary: user -> {success, failed}
-user_stats = {}
-totalSuccess = 0
-totalFailed  = 0
 
-for log in login_logs:
-    user = log[0]
-    status = log[2]
+def getLogDetails():
+    # Dictionary: user -> {success, failed}
+    user_stats = {}
+    totalSuccess = 0
+    totalFailed  = 0
 
-    # Initialize user if not present
-    if user not in user_stats:
-        user_stats[user] = {"success": 0, "failed": 0}
+    for log in login_logs:
+        user = log[0]
+        status = log[2]
 
-    # Update counts
-    if status == "success":
-        user_stats[user]["success"] += 1
-        totalSuccess += 1
-    else:
-        user_stats[user]["failed"] += 1
-        totalFailed += 1
+        # Initialize user if not present
+        if user not in user_stats:
+            user_stats[user] = {"success": 0, "failed": 0}
 
+        # Update counts
+        if status == "success":
+            user_stats[user]["success"] += 1
+            totalSuccess += 1
+        else:
+            user_stats[user]["failed"] += 1
+            totalFailed += 1
+    return totalSuccess, totalFailed, user_stats
+
+def suspectDetector():
+    userSpamRecord = {}
+    for log in login_logs:
+        user, ip, status = log
+        if user not in userSpamRecord:
+            userSpamRecord[user] = {
+                'ips':[],
+                'failedAttempts': 0,
+                'successAttempts': 0,
+                'userRiskScore': -3
+            }
+        if ip not in userSpamRecord[user]["ips"]:
+            userSpamRecord[user]['ips'].append(ip)
+            userSpamRecord[user]['userRiskScore'] += 3
+        if status == 'failed':
+            userSpamRecord[user]['failedAttempts'] += 1
+            userSpamRecord[user]['userRiskScore'] += 2
+        elif status == 'success':
+            userSpamRecord[user]['successAttempts'] += 1
+            userSpamRecord[user]['userRiskScore'] += 1
+    return userSpamRecord
+
+
+# -------------------------Task1--------------------------
+totalSuccess, totalFailed,  user_stats = getLogDetails()
 # Output
 print("Login Summary Per User:")
 print ("Total Success Logins:",totalSuccess)
@@ -47,31 +75,10 @@ print("Total Failed Logins:", totalFailed)
 for user, stats in user_stats.items():
     print(user, "-> Success:", stats["success"], ", Failed:", stats["failed"])
 
-# -------------------------Task2--------------------------
-userSpamRecord = {}
-for log in login_logs:
-    user, ip, status = log
-    if user not in userSpamRecord:
-        userSpamRecord[user] = {
-            'ips':[],
-            'failedAttempts': 0,
-            'successAttempts': 0,
-            'userRiskScore': -3
-        }
-    if ip not in userSpamRecord[user]["ips"]:
-        userSpamRecord[user]['ips'].append(ip)
-        userSpamRecord[user]['userRiskScore'] += 3
-    if status == 'failed':
-        userSpamRecord[user]['failedAttempts'] += 1
-        userSpamRecord[user]['userRiskScore'] += 2
-    elif status == 'success':
-        userSpamRecord[user]['successAttempts'] += 1
-        userSpamRecord[user]['userRiskScore'] += 1
-
-
 
 print()
 print("---------------------------Task2----------------------------")
+userSpamRecord = suspectDetector()
 for user, stats in userSpamRecord.items():
     if len(stats['ips']) > 1:
         print(f"{user} → IP Hopping Suspect")
@@ -81,6 +88,7 @@ for user, stats in userSpamRecord.items():
          
 print()
 print("---------------------------Task3----------------------------")
+
 for user, stats in userSpamRecord.items():
     print(f"{user} : {stats['userRiskScore']}")
     #print(f"Success : {stats['successAttempts']}")
